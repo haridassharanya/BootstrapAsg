a04722eeea0b3b02dbac48407525af6e1dd63d3d
Sharanya Haridas <sharanya0211@gmail.com>
2014-04-13 21:30:10 -0700

    basic js

---
+++ script.js
@@ -0,0 +1,24 @@
+/**
+ * @author Sharanya Haridas
+ */
+
+var video = "<iframe width=560' height='315' src='http://www.youtube.com/embed/rARN6agiW7o?html5=1' frameborder='0' allowfullscreen></iframe>";
+
+$(document).ready(function() {
+	setNav();
+});
+
+
+function setNav() {
+
+	$(".gatsby1").on("click", function() {
+		$('#myModal').modal();
+		$(".modal-body").html(video);
+	});	
+
+
+	$('#myModal').on('hidden.bs.modal', function () {
+		$('#myModal iframe').removeAttr('src');
+	});
+
+}
